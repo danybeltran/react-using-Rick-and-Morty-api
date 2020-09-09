14874f94678af24606a134afd1e14749cce44e5d
azamidemika <josuebeltran_2010@hotmail.com>
2020-09-09 11:34:57 -0600

    Initial commit - Set up project, add navigation

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./tailwind.scss"
+import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Navigation } from './components/Navigation';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+            <Navigation/>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    purge: [],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        backgroundColor: ["responsive", "active", "focus", "hover"]
+    },
+    plugins: [],
+}
