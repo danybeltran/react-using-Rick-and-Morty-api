c73d0d2f23d8106075582580faed9197b6c39cd7
azamidemika <josuebeltran_2010@hotmail.com>
2020-09-11 00:48:18 -0600

    Make morty's icon more visible in Favorite screen

--- src/components/Favorite/NoResults.jsx
+++ src/components/Favorite/NoResults.jsx
@@ -1,15 +1,16 @@
 import React, { useContext } from "react";
-import MortyFace from "../../img/morty-no-favorites.png";
+import MortyFace from "../../img/morty-no-favorites.svg";
+import MortyFaceDarkTheme from "../../img/morty-no-favorites-dark.svg";
 import { AppContext } from "../../Context";
 
 export const NoResults = () => {
 
-    const { favoriteCharacters, fetchFavoriteDone } = useContext(AppContext);
+    const { favoriteCharacters, fetchFavoriteDone, darkTheme } = useContext(AppContext);
 
     return favoriteCharacters.length === 0 && fetchFavoriteDone ? (
         <div className="favorite-b">
             <div className="container-no-results">
-                <img src={MortyFace} className="no-results-image" alt="" />
+                <img src={darkTheme ? MortyFaceDarkTheme : MortyFace} className="no-results-image" alt="" />
                 <h3 className="no-results-text">List is empty</h3>
             </div>
         </div>
